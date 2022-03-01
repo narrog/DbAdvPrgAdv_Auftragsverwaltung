6d5cae593d791e7f3b4bc208541a48d451bd1321
Benjamin Peter <benjaminpeter@outlook.com>
2022-03-01 15:56:14 +0100

    Window für YoY erstellt

---
+++ DbAdvPrgAdv_Auftragsverwaltung/ViewModel/YoyViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace DbAdvPrgAdv_Auftragsverwaltung.ViewModel
+{
+    public class YoyViewModel : INotifyPropertyChanged
+    {
+        private List<string> windowFuncResult;
+        public YoyViewModel()
+        {
+            var testList = new List<string>();
+            testList.Add("first");
+            testList.Add("Second");
+            WindowFuncResult = testList;
+            Close = new CommandClose();
+        }
+        public List<string> WindowFuncResult
+        { 
+            get { return windowFuncResult; }
+            set
+            {
+                windowFuncResult = value;
+                OnProptertyChanged(nameof(WindowFuncResult));
+            }
+        }
+        public ICommand Close { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnProptertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
