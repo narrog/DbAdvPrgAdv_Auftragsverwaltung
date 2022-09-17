7ee43f00bda902ba562443b6168bc9b991743c45
leonieb1 <me.leoniebuser.ch@zbw-online.ch>
2022-09-17 13:42:01 +0200

    Order Repo-Versuch

--- DbAdvPrgAdv_Auftragsverwaltung/Form/EditOrder.xaml.cs
+++ DbAdvPrgAdv_Auftragsverwaltung/Form/EditOrder.xaml.cs
@@ -6,6 +6,7 @@ using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
 using DbAdvPrgAdv_Auftragsverwaltung.Model;
+using DbAdvPrgAdv_Auftragsverwaltung.ViewModel;
 using Microsoft.EntityFrameworkCore;
 
 namespace DbAdvPrgAdv_Auftragsverwaltung.Form {
@@ -13,30 +14,39 @@ namespace DbAdvPrgAdv_Auftragsverwaltung.Form {
     /// Interaction logic for EditOrder.xaml
     /// </summary>
     public partial class EditOrder : Window {
+
+        private readonly OrderVM _orderVM;
         public EditOrder(MainWindow mainWindow, Order selected) {
             InitializeComponent();
             Main = mainWindow;
             SelectedOrder = selected;
             this.DataContext = this;
+            _orderVM = new OrderVM();
 
-            using (var context = new OrderContext())
-            {
-                if (SelectedOrder.OrderID == 0)
-                {
+            using (var context = new OrderContext()) {
+                if (SelectedOrder.OrderID == 0) {
                     Number = 1;
                 }
-                else
-                {
+                else {
                     Number = SelectedOrder.Positions.Count + 1;
                 }
                 Customers = context.Customers.ToList();
             }
 
+            // *** Versuch Context zu ersetzen ***/
+            //
+            //if (SelectedOrder.OrderID == 0) {
+            //    Number = 1;
+            //}
+            //else {
+            //    Number = SelectedOrder.Positions.Count + 1;
+            //}
+            //Customers = _orderVM.GetAllCustomers();
+
             // CmbBox Customers füllen
             foreach (var item in Customers) {
                 CmbCustomer.Items.Add(item.CustomerID + " " + item.Name + " " + item.Vorname);
-                if (item.CustomerID == SelectedOrder.CustomerID)
-                {
+                if (item.CustomerID == SelectedOrder.CustomerID) {
                     CmbCustomer.SelectedItem = item.CustomerID + " " + item.Name + " " + item.Vorname;
                 }
             }
@@ -47,16 +57,17 @@ namespace DbAdvPrgAdv_Auftragsverwaltung.Form {
         public List<Customer> Customers { get; set; }
         public int Number { get; set; }
 
-        private void CmdAbort_OnClick(object sender, RoutedEventArgs e)
-        {
+        private void CmdAbort_OnClick(object sender, RoutedEventArgs e) {
             Main.UpdateGrid();
             Close();
         }
 
-        private void CmdSave_OnClick(object sender, RoutedEventArgs e)
-        {
-            using (var context = new OrderContext())
-            {
+        private void CmdSave_OnClick(object sender, RoutedEventArgs e) {
+            // *** Versuch Context zu ersetzen ***/
+            //SelectedOrder.OrderDate = DateTime.Now;
+            //_orderVM.UpdateOrder(SelectedOrder);
+
+            using (var context = new OrderContext()) {
                 SelectedOrder.OrderDate = DateTime.Now;
                 context.Orders.Update(SelectedOrder);
                 context.SaveChanges();
@@ -65,31 +76,35 @@ namespace DbAdvPrgAdv_Auftragsverwaltung.Form {
             Close();
         }
 
-        private void CmdAddPos_Click(object sender, RoutedEventArgs e)
-        {
-            using (var context = new OrderContext())
-            {
+        private void CmdAddPos_Click(object sender, RoutedEventArgs e) {
+            // *** Versuch Context zu ersetzen ***/
+            //SelectedOrder.OrderDate = DateTime.Now;
+            //var id = Convert.ToInt32(CmbCustomer.Text.Split()[0]);
+            //SelectedOrder.Customer = _orderVM.GetCustomerById(id);
+            //if (SelectedOrder.OrderID == 0)
+            //    _orderVM.AddOrder(SelectedOrder);
+            //else
+            //    _orderVM.UpdateOrder(SelectedOrder);
+            //SelectedOrder = _orderVM.GetOrderById(SelectedOrder.OrderID);
+
+            using (var context = new OrderContext()) {
                 SelectedOrder.OrderDate = DateTime.Now;
                 var id = Convert.ToInt32(CmbCustomer.Text.Split()[0]);
                 SelectedOrder.Customer = context.Customers.Find(id);
-                if (SelectedOrder.OrderID == 0)
-                {
+                if (SelectedOrder.OrderID == 0) {
                     context.Orders.Add(SelectedOrder);
                 }
-                else
-                {
+                else {
                     context.Orders.Update(SelectedOrder);
                 }
                 context.SaveChanges();
                 SelectedOrder = context.Orders.Find(SelectedOrder.OrderID);
             }
-            var windowPosition = new EditPosition(this, new Position() {Article = new Article(), Number = this.Number, Order = SelectedOrder, OrderID = SelectedOrder.OrderID}, true);
+            var windowPosition = new EditPosition(this, new Position() { Article = new Article(), Number = this.Number, Order = SelectedOrder, OrderID = SelectedOrder.OrderID }, true);
             windowPosition.Show();
         }
-        private void CmdDelPos_Click(object sender, RoutedEventArgs e)
-        {
-            using (var context = new OrderContext())
-            {
+        private void CmdDelPos_Click(object sender, RoutedEventArgs e) {
+            using (var context = new OrderContext()) {
                 var selected = (Position)GrdPositions.SelectedItem;
                 var toDelete = context.Positions
                     .FirstOrDefault(x => x.ArticleID == selected.ArticleID && x.OrderID == selected.OrderID);
@@ -100,18 +115,26 @@ namespace DbAdvPrgAdv_Auftragsverwaltung.Form {
             }
         }
 
-        public void UpdateGrid()
-        {
-            using (var context = new OrderContext())
-            {
+        public void UpdateGrid() {
+            // *** Versuch Context zu ersetzen ***/
+            //SelectedOrder = _orderVM.GetOrderById(SelectedOrder.OrderID);
+            //GrdPositions.ItemsSource = SelectedOrder.Positions.ToList();
+            //double sum = 0;
+            //foreach (var item in SelectedOrder.Positions) {
+            //    sum = sum + item.Article.Price * item.Count;
+            //}
+            //SelectedOrder.PriceTotal = sum;
+            //TxtPriceTotal.Text = SelectedOrder.PriceTotal.ToString();
+            //_orderVM.UpdateOrder(SelectedOrder);
+
+            using (var context = new OrderContext()) {
                 SelectedOrder = context.Orders
                     .Include("Positions")
                     .FirstOrDefault(x => x.OrderID == SelectedOrder.OrderID);
 
                 GrdPositions.ItemsSource = SelectedOrder.Positions.ToList();
                 double sum = 0;
-                foreach (var item in SelectedOrder.Positions)
-                {
+                foreach (var item in SelectedOrder.Positions) {
                     sum = sum + item.Article.Price * item.Count;
                 }
                 SelectedOrder.PriceTotal = sum;
---
+++ DbAdvPrgAdv_Auftragsverwaltung/ViewModel/OrderVM.cs
@@ -0,0 +1,34 @@
+﻿using DbAdvPrgAdv_Auftragsverwaltung.Model;
+using DbAdvPrgAdv_Auftragsverwaltung.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbAdvPrgAdv_Auftragsverwaltung.ViewModel {
+    internal class OrderVM {
+        private readonly OrderRepository _orderRep;
+        private readonly CustomerRepository _customerRep;
+
+        public OrderVM() {
+            _orderRep = new OrderRepository();
+            _customerRep = new CustomerRepository();
+        }
+        public List<Customer> GetAllCustomers() {
+            return _customerRep.GetAll();
+        }
+        public Customer GetCustomerById(int id) {
+            return _customerRep.GetById(id);
+        }
+        public Order GetOrderById(int id) {
+            return _orderRep.GetById(id);
+        }
+        public void AddOrder(Order order) {
+            _orderRep.Add(order);
+        }
+        public void UpdateOrder(Order order) {
+            _orderRep.Add(order);
+        }
+    }
+}
